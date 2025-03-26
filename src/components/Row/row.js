2f548765bf0f4896b4506cc68c3d249aba13a328
CKAMMEP <dden92964@gmail.com>
2025-03-26 10:25:38 +0500

    #20 default props and proptypes

--- src/components/Row/row.js
+++ src/components/Row/row.js
@@ -1,5 +1,5 @@
 import React from "react"
-
+import PropTypes from 'prop-types'
 const Row = ({ left, right }) => {
     return (
         <div className="row mb2">
@@ -12,6 +12,11 @@ const Row = ({ left, right }) => {
         </div>
     )
 }
+Row.propTypes = {
+    left: PropTypes.node,
+    right: PropTypes.node
+}
+
 
 export default Row
 
